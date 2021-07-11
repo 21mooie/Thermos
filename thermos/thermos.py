27817e9876b6e8b960dacb7998b52abc9c7e5785
Muata Nkosi <muatankosi1@gmail.com>
2021-07-11 15:41:05 -0400

    Implement logic to show data from db in app and add manage script Manage script is not working because of circular imports but I do not feel like investing more time to solve

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 from forms import BookmarkForms
+import models
 
 app = Flask(__name__)
 
@@ -14,22 +15,11 @@ app.config['SECRET_KEY'] = b"\x1f\xd59d\xfb2N\xc7\x96\x93\xa18#\x99\xe3(\xd4'8%4
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 db = SQLAlchemy(app)
 
-def store_bookmark(url, description):
-    bookmarks.append(dict(
-        url = url,
-        user = "muata",
-        description = description,
-        date = datetime.utcnow()
-    ))
-
-def new_bookmarks(num):
-    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html',
-    new_bookmarks=new_bookmarks(5)
+    new_bookmarks=models.Bookmark.newest(5)
     )
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -38,7 +28,9 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmark(url, description)
+        bm = models.Bookmark(url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         flash("Stored bookmark '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
